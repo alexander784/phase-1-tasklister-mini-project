26bb36953a15562d783c44ab6849f73407f87427
alexander <alexandernyaga1999@gmail.com>
2023-10-10 05:13:56 +0300

    prevent default

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form =document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault ()
+    const formInput = e.target.querySelector ("input[type='text']").value;
+    console.log(formInput);
+  })
 });

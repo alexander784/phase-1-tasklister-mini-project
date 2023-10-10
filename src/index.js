50e96bcb7e14c91115849224bb7680d5b1624ae2
alexander <alexandernyaga1999@gmail.com>
2023-10-10 05:25:26 +0300

    append button

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault ()
     const formInput = e.target.querySelector ("input[type='text']").value;
     console.log(formInput);
+    listTodo(formInput)
 
   })
 });
 function listTodo(Todo) {
   let li = document.createElement('li');
-
+  let btn = document.createElement('button');
+  btn.addEventListener('click', function(e){
+    e.target.parentNode.remove
+  })
+  btn.textContent = 'x'
+  li.textContent = `${Todo} `;
+  li.appendChild(btn)
   li.textContent = Todo;
   document.querySelector('#tasks').appendChild(li)
   

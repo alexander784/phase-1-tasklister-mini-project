e17017555c88ab18d12146d65da2ffa22831b57b
alexander <alexandernyaga1999@gmail.com>
2023-10-10 05:16:38 +0300

    add Todo function

--- src/index.js
+++ src/index.js
@@ -5,5 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault ()
     const formInput = e.target.querySelector ("input[type='text']").value;
     console.log(formInput);
+
   })
 });
+function listTodo(Todo) {
+  let li = document.createElement('li');
+
+  li.textContent = Todo;
+  document.querySelector('#tasks').appendChild(li)
+  
+}
